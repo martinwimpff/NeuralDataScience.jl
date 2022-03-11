d90921e05d60719ed096503b840d46fade0ea473
Documenter.jl <documenter@juliadocs.github.io>
2022-03-11 15:53:43 +0000

    build based on 5f04394

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = NeuralDataScience","category":"page"},{"location":"#NeuralDataScience","page":"Home","title":"NeuralDataScience","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for NeuralDataScience.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [NeuralDataScience]","category":"page"}]
+}
